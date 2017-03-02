587383eec58ec9cd35ae28327f944af63305214e
Thomas Kaio <thomaskaio@hotmail.com>
2017-03-02 16:06:56 -0600

    added jquery to afterFunctions

--- afterFunctions.js
+++ afterFunctions.js
@@ -1,4 +1,39 @@
-function writeHTML(){
+javascript:(function(e,a,g,h,f,c,b,d,p,k,l,m){
+
+	if(!(f=e.jQuery)||g>f.fn.jquery||h(f)){
+
+		c=a.createElement("script");
+		c.type="text/javascript";
+		c.src="//ajax.googleapis.com/ajax/libs/jquery/"+g+"/jquery.min.js";
+		c.onload=c.onreadystatechange=function(){
+
+			if(!b&&(!(d=this.readyState)||d=="loaded"||d=="complete")){
+
+				p=a.createElement("script");
+				p.type="text/javascript";
+				p.src="//ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js";
+				p.onload=p.onreadystatechange=function(){
+
+					if(!k&&(!(l=this.readyState)||l=="loaded"||l=="complete")){
+
+
+						h((f=e.jQuery).noConflict(1), b=1, k = 1);
+						a.documentElement.childNodes[0].removeChild(c);
+						a.documentElement.childNodes[0].removeChild(p);
+						p.onload=p.onreadystatechange=null;
+						c.onload=c.onreadystatechange=null;
+
+					}
+				}
+				a.documentElement.childNodes[0].appendChild(p);
+			}
+		};
+		a.documentElement.childNodes[0].appendChild(c);
+		}
+})
+
+(window,document,"3.1.1",function($, N, O){
+	function writeHTML(){
 	/*Creating HTML*/
 	newPage = "<html lang='en'>";
 	newPage += "<head>";
@@ -72,3 +107,4 @@ function writeHTML(){
 			});
 		}
 	}
+})
