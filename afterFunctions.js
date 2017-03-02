580cd16e4d7fb760a848204ccdd4ddb7c2d324df
Thomas Kaio <thomaskaio@hotmail.com>
2017-03-02 16:09:10 -0600

    removed jquery from afterFunctions

--- afterFunctions.js
+++ afterFunctions.js
@@ -1,39 +1,4 @@
-javascript:(function(e,a,g,h,f,c,b,d,p,k,l,m){
-
-	if(!(f=e.jQuery)||g>f.fn.jquery||h(f)){
-
-		c=a.createElement("script");
-		c.type="text/javascript";
-		c.src="//ajax.googleapis.com/ajax/libs/jquery/"+g+"/jquery.min.js";
-		c.onload=c.onreadystatechange=function(){
-
-			if(!b&&(!(d=this.readyState)||d=="loaded"||d=="complete")){
-
-				p=a.createElement("script");
-				p.type="text/javascript";
-				p.src="//ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js";
-				p.onload=p.onreadystatechange=function(){
-
-					if(!k&&(!(l=this.readyState)||l=="loaded"||l=="complete")){
-
-
-						h((f=e.jQuery).noConflict(1), b=1, k = 1);
-						a.documentElement.childNodes[0].removeChild(c);
-						a.documentElement.childNodes[0].removeChild(p);
-						p.onload=p.onreadystatechange=null;
-						c.onload=c.onreadystatechange=null;
-
-					}
-				}
-				a.documentElement.childNodes[0].appendChild(p);
-			}
-		};
-		a.documentElement.childNodes[0].appendChild(c);
-		}
-})
-
-(window,document,"3.1.1",function($, N, O){
-	function writeHTML(){
+function writeHTML(){
 	/*Creating HTML*/
 	newPage = "<html lang='en'>";
 	newPage += "<head>";
@@ -107,4 +72,3 @@ javascript:(function(e,a,g,h,f,c,b,d,p,k,l,m){
 			});
 		}
 	}
-})
