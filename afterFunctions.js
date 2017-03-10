f5f2d7ff9f6a64b76c4d9a841bbc7ca0707ceb4f
Thomas Kaio <thomaskaio@hotmail.com>
2017-03-10 16:16:55 -0600

    test

--- afterFunctions.js
+++ afterFunctions.js
@@ -76,7 +76,7 @@ function writeHTML(){
 					tagPosition = $(this).offset();
 					leftside = Math.round(tagPosition.left)+"px";
 					topside = Math.round(tagPosition.top)+"px";
-					appendWarning = $("<div class='testing' id='"+parsedimg[i]+"_warning' style='position: absolute; top: "+topside+"; left: "+leftside+"; background-color: yellow; z-index: 9999;'>top: "+leftside+"; left: "+topside+";</div>");
+					appendWarning = $("<div class='testing' id='"+parsedimg[i]+"_warning' style='top: "+topside+"; left: "+leftside+"; background-color: yellow; z-index: 9999;'>top: "+leftside+"; left: "+topside+";</div>");
 					$("body").append(appendWarning);
 				}
 				else{
