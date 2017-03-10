cc46b81bc19b76ebbf8fbf1404cdd039dd03adec
Thomas Kaio <thomaskaio@hotmail.com>
2017-03-10 13:15:13 -0600

    test

--- accCheck.js
+++ accCheck.js
@@ -139,7 +139,7 @@ javascript:(function(e,a,g,h,f,c,b,d,p,k,l,m){
 				test = $("<div id='coco'>top: "+leftside+"; left: "+topside+"; " + missingAttr + " = " + $(this).attr(missingAttr) + "</div>");
 				$("html").append(test);
 
-				$("#coco").css({top: topside, left: leftside, position:'fixed', background: "yellow"});
+				$("#coco").css({top: "100px", left: leftside, position:'fixed', background: "yellow"});
 				attrFound = idList;
 			}
 			totalItemsCounter++;
