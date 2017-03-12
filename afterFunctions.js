69e56bb71bbbe9a990a3c34c67eb7cc4c1b2bd70
Thomas Kaio <thomaskaio@hotmail.com>
2017-03-12 12:52:40 -0500

    test

--- afterFunctions.js
+++ afterFunctions.js
@@ -72,7 +72,7 @@ function writeHTML(){
 
 		for(var i = 0; i < parsedimg.length; i++) {
 			$("#" + parsedimg[i]).each(function(){
-				$(this).append("<div class='tesing'>test</div>");
+				$("<div class='testing'>test</div>")insertBefore(this);
 				/*if(!$("#"+parsedimg[i]+"_warning").length){
 					tagPosition = $(this).offset();
 					leftside = "-"+Math.round($(this).height())+"px";
