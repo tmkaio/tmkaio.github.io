891c0860031aeb0524c8f05d448da6c0d025b963
Thomas Kaio <thomaskaio@hotmail.com>
2017-03-13 08:23:38 -0500

    test

--- afterFunctions.js
+++ afterFunctions.js
@@ -76,7 +76,7 @@ function writeHTML(){
 
 				if(!$("#"+parsedimg[i]+"_warning").length && $(this).prop("tagName") != "html"){
 
-					$(this).wrap("<div id='"+parsedimg[i]+"_warning' class='warningTag'></div>");
+					$(this).wrap("<div id='"+parsedimg[i]+"_warning' style='max-width:"+$(this).width()+"px; max-height:"+$(this).height()+"px;' class='warningTag'></div>");
 					$("#"+parsedimg[i]+"_warning").prepend($(this).prop("tagName"));
 
 				}
