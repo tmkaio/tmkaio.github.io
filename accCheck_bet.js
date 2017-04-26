90b6ee7aa49331a52beaaa2d1938fd3f880c7f8b
Thomas Kaio <thomaskaio@hotmail.com>
2017-04-26 10:41:45 -0500

    sadf

--- accCheck_bet.js
+++ accCheck_bet.js
@@ -248,10 +248,12 @@ function findLinkedTag(tag1, attr1, tag2, attr2){
 
 
     if($(this).attr(attr1).lenght){
+      alert(totalItemsCounter+"A");
       labelString = "$(place).find('" + tag2 + "[" + attr2 + "=" + $(this).attr(attr1).replace( /(:|\.|\[|\]|,|=|@)/g, "\\$1" ) + "]')";
       exlabelString = eval(labelString);
     }
     else{
+      alert(totalItemsCounter+"B");
       labelString = "$(place).find('" + tag2 + "[" + attr2 + "=" + $(this).attr(attr1) + "]')";
       exlabelString = eval(labelString);
     }
