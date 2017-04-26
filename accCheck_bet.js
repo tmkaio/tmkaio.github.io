d9303f0a8783f42bb9f672bc0ab951459bbcf672
Thomas Kaio <thomaskaio@hotmail.com>
2017-04-26 10:29:00 -0500

    asdf

--- accCheck_bet.js
+++ accCheck_bet.js
@@ -245,8 +245,6 @@ function findLinkedTag(tag1, attr1, tag2, attr2){
 
 	$(place).find(tag1).each(function(){
 
-
-    alert($(this).attr(attr1).replace( /(:|\.|\[|\]|,|=|@)/g, "\\$1" ));
 		if(!$(place).find(tag2 + "[" + attr2 + "=" + $(this).attr(attr1).replace( /(:|\.|\[|\]|,|=|@)/g, "\\$1" ) + "]").length){
 
 			checkExistingID(this,tag1,itemsCounter);
