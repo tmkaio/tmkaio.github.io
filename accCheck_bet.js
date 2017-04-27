ae2deab772b7b9a11c316f1955ee33ee7f11da63
Thomas Kaio <thomaskaio@hotmail.com>
2017-04-27 08:38:13 -0500

    df

--- accCheck_bet.js
+++ accCheck_bet.js
@@ -763,6 +763,7 @@ function selectIFrame(){
 		finalCounters = "";
 		finalDuplicatedIDs = "";
     $(this).off("click");
+    $("iframe").off('mouseenter mouseleave');
     $(this).find("html").css({border: "5px solid green"});
 		application(this);
 		//$(this).append(accCheckAppend);
